19087f7d29854b80a20b2f378a9bdb8c7f72886d
Arif <faizin.arief@gmail.com>
2020-05-22 19:20:53 +0700

    Fix: rearrange

--- app/src/main/java/com/dicoding/myreactivesearch/MainViewModel.kt
+++ app/src/main/java/com/dicoding/myreactivesearch/MainViewModel.kt
@@ -12,17 +12,18 @@ import kotlinx.coroutines.flow.*
 @ExperimentalCoroutinesApi
 class MainViewModel : ViewModel() {
 
-    private val accessToken = "pk.eyJ1IjoiYXJpZmFpemluIiwiYSI6ImNrYTI2c3R1cjAwNXAzbm1zaDYyZW1ra2cifQ.okSWF0zf58rWkhoVuYjShQ"
+    private val accessToken =
+        "pk.eyJ1IjoiYXJpZmFpemluIiwiYSI6ImNrYTI2c3R1cjAwNXAzbm1zaDYyZW1ra2cifQ.okSWF0zf58rWkhoVuYjShQ"
     val queryChannel = BroadcastChannel<String>(Channel.CONFLATED)
 
     val searchResult = queryChannel.asFlow()
-            .debounce(300)
-            .filter {
-                it.trim().isEmpty().not()
-            }
-            .distinctUntilChanged()
-            .mapLatest {
-                ApiConfig.provideApiService().getCountry(it, accessToken).features
-            }
-            .asLiveData()
+        .debounce(300)
+        .distinctUntilChanged()
+        .filter {
+            it.trim().isEmpty().not()
+        }
+        .mapLatest {
+            ApiConfig.provideApiService().getCountry(it, accessToken).features
+        }
+        .asLiveData()
 }
